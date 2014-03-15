862b73445cdca9195739e234d6185f362ac43072
bvenners <bill@artima.com>
2014-03-15 12:45:20 -0700

    Use PlaySpec in UnitSpec.

--- src/test/scala/org/scalatestplus/play/UnitSpec.scala
+++ src/test/scala/org/scalatestplus/play/UnitSpec.scala
@@ -21,5 +21,5 @@ import org.scalatest._
 /*
  * Play-Test super-suite for basic unit tests.
 */
-abstract class UnitSpec extends WordSpec with MustMatchers with OptionValues with Inside
+abstract class UnitSpec extends PlaySpec with OptionValues with Inside
 
