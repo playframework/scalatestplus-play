517ba46605ff1b23f9a99c2d54b996acce6416e4
cheeseng <cheeseng@amaseng.com>
2014-03-25 21:58:29 +0800

    Changed AllBrowsersPerTest to use shared tests approach.

--- src/main/scala/org/scalatestplus/play/AllBrowsersPerTest.scala
+++ src/main/scala/org/scalatestplus/play/AllBrowsersPerTest.scala
@@ -18,35 +18,156 @@ package org.scalatestplus.play
 import play.api.test._
 import org.scalatest._
 import org.scalatest.events._
+import org.scalatest.tags._
 import selenium.WebBrowser
 import concurrent.Eventually
 import concurrent.IntegrationPatience
 import org.openqa.selenium.WebDriver
-import BrowserDriver.NoDriver
+import BrowserDriver.{NoDriver, WithoutDriver}
 import org.openqa.selenium.firefox.FirefoxProfile
 
 /**
  * Trait that provides new browser instances (for all browsers available on the running platform) for each test executed in a ScalaTest <code>Suite</code>.
  *
- * It overrides ScalaTest's <code>withFixture</code>, <code>runTest</code> and <code>runTests</code> method to create new <code>WebDriver</code>, <code>TestServer</code> and
+ * It overrides ScalaTest's <code>withFixture</code> and <code>runTest</code> method to create new <code>WebDriver</code>, <code>TestServer</code> and
  * <code>FakeApplication</code> instance before executing each test.
  *
- * You can explicitly specify which <code>WebBrowser</code>(s) to run though config map's <code>"browsers"</code> key:
+ * For tests that need to be shared between multiple browsers, you'll need to put them in <code>registerSharedTests</code> and append <code>forBrowser.name</code>
+ * to the test name.  All tests registered under <code>registerSharedTests</code> will be provided with real <code>WebDriver</code> available on the system.
+ * For unavailable <code>WebDriver</code> on the running platform, the test will be canceled.  All tests registered under <code>registerSharedTests</code> will be
+ * tagged automatically, when the test name ends with [Firefox] (returned from <code>forBrowser.name</code> when <code>WebDriver</code> is <code>FirefoxDriver</code>),
+ * the test will be automatically tagged with "org.scalatest.tags.Firefox".  This means that you can include/exclude tests using ScalaTest's tagging feature.
  *
- * <ul>
- *   <li>C - Chrome</li>
- *   <li>F - Firefox</li>
- *   <li>I - Internet Explorer</li>
- *   <li>S - Safari</li>
- *   <li>H - HtmlUnit</li>
- * </ul>
- *
- * For example, you can pass in <code>-Dbrowsers="CF"</code> to run <code>Chrome</code> and <code>Firefox</code> only.
- *
- * If no valid web browser is specified through config map's <code>"browsers"</code> string, it will fallback to default which discover all available <code>WebDriver</code>
- * and run with them.
  */
 trait AllBrowsersPerTest extends SuiteMixin with WebBrowser with Eventually with IntegrationPatience { this: Suite =>
+
+  /**
+   * Indicate whether to register tests for Firefox, by default returns <code>true</code>.  You can override it to return <code>false</code>
+   * to always disable tests for Firefox.
+   */
+  lazy val registerSharedTestsForFirefox = true
+
+  /**
+   * Indicate whether to register tests for Safari, by default returns <code>true</code>.  You can override it to return <code>false</code>
+   * to always disable tests for Safari.
+   */
+  lazy val registerSharedTestsForSafari = true
+
+  /**
+   * Indicate whether to register tests for Internet Explorer, by default returns <code>true</code>.  You can override it to return <code>false</code>
+   * to always disable tests for Internet Explorer.
+   */
+  lazy val registerSharedTestsForInternetExplorer = true
+
+  /**
+   * Indicate whether to register tests for Chrome, by default returns <code>true</code>.  You can override it to return <code>false</code>
+   * to always disable tests for Chrome.
+   */
+  lazy val registerSharedTestsForChrome = true
+
+  /**
+   * Indicate whether to register tests for HtmlUnit, by default returns <code>true</code>.  You can override it to return <code>false</code>
+   * to always disable tests for HtmlUnit.
+   */
+  lazy val registerSharedTestsForHtmlUnit = true
+
+  /**
+   * Method to provide <code>FirefoxProfile</code> for creating <code>FirefoxDriver</code>, you can override this method to
+   * provide a customized instance of <code>FirefoxProfile</code>
+   *
+   * @return an instance of <code>FirefoxProfile</code>
+   */
+  protected def firefoxProfile: FirefoxProfile = new FirefoxProfile
+
+  // Not sealed on purpose, so people can extend it if other
+  // Browser driver types appear (or we could just use strings
+  // for the browser names)
+  /**
+   * Abstract super class to represent a browser.
+   *
+   * @param name the browser name
+   * @param tagName the browser tag name
+   */
+  abstract class ForBrowser(val name: String, val tagName: String) {
+    /**
+     * Create <code>WebDriver</code> instance for the represented browser.
+     *
+     * @return <code>WebDriver</code> instance for the represented browser
+     */
+    def createWebDriver: WebDriver
+  }
+
+  /**
+   * Case object for Firefox browser.
+   */
+  case object ForFirefox extends ForBrowser(" [Firefox]", "org.scalatest.tags.Firefox") {
+    /**
+     * Create <code>FirefoxDriver</code> instance.
+     *
+     * @return <code>FirefoxDriver</code> instance
+     */
+    def createWebDriver: WebDriver = WebDriverFactory.createFirefoxDriver(firefoxProfile)
+  }
+
+  /**
+   * Case object for Safari browser.
+   */
+  case object ForSafari extends ForBrowser(" [Safari]", "org.scalatest.tags.Safari") {
+    /**
+     * Create <code>SafariDriver</code> instance.
+     *
+     * @return <code>SafariDriver</code> instance
+     */
+    def createWebDriver: WebDriver = WebDriverFactory.createSafariDriver
+  }
+
+  /**
+   * Case object for Internet Explorer browser.
+   */
+  case object ForInternetExplorer extends ForBrowser(" [InternetExplorer]", "org.scalatest.tags.InternetExplorer") {
+    /**
+     * Create <code>InternetExplorerDriver</code> instance.
+     *
+     * @return <code>InternetExplorerDriver</code> instance
+     */
+    def createWebDriver: WebDriver = WebDriverFactory.createInternetExplorerDriver
+  }
+
+  /**
+   * Case object for Chrome browser.
+   */
+  case object ForChrome extends ForBrowser(" [Chrome]", "org.scalatest.tags.Chrome") {
+    /**
+     * Create <code>ChromeDriver</code> instance.
+     *
+     * @return <code>ChromeDriver</code> instance
+     */
+    def createWebDriver: WebDriver = WebDriverFactory.createChromeDriver
+  }
+
+  /**
+   * Case object for HtmlUnit browser.
+   */
+  case object ForHtmlUnit extends ForBrowser(" [HtmlUnit]", "org.scalatest.tags.HtmlUnit") {
+    /**
+     * Create <code>HtmlUnitDriver</code> instance.
+     *
+     * @return <code>HtmlUnitDriver</code> instance
+     */
+    def createWebDriver: WebDriver = WebDriverFactory.createHtmlUnitDriver
+  }
+
+  /**
+   * Available browsers, you can override to add in your custom <code>ForBrowser</code> implementation.
+   */
+  protected val browsers: IndexedSeq[ForBrowser] =
+    Vector(
+      ForFirefox,
+      ForSafari,
+      ForInternetExplorer,
+      ForChrome,
+      ForHtmlUnit
+    )
 
   private var privateApp: FakeApplication = _
 
@@ -61,25 +182,9 @@ trait AllBrowsersPerTest extends SuiteMixin with WebBrowser with Eventually with
    */
   val port: Int = Helpers.testServerPort
 
-  /**
-   * Method to provide <code>FirefoxProfile</code> for creating <code>FirefoxDriver</code>, you can override this method to
-   * provide a customized instance of <code>FirefoxProfile</code>
-   *
-   * @return an instance of <code>FirefoxProfile</code>
-   */
-  protected def firefoxProfile: FirefoxProfile = new FirefoxProfile
-
-  private val webDrivers: Set[(String, () => WebDriver)] =
-    Set(
-      ("Chrome", () => WebDriverFactory.createChromeDriver),
-      ("Firefox", () => WebDriverFactory.createFirefoxDriver(firefoxProfile)),
-      ("Internet Explorer", () => WebDriverFactory.createInternetExplorerDriver),
-      ("Safari", () => WebDriverFactory.createSafariDriver),
-      ("HtmlUnit", () => WebDriverFactory.createHtmlUnitDriver)
-    )
-
-  private var privateDriverMeta: (String, () => WebDriver) = _
   private var privateWebDriver: WebDriver = _
+
+  private var privateWebDriverName: String = _
 
   /**
    * Implicit method to get the <code>WebDriver</code> for the current test.
@@ -87,20 +192,54 @@ trait AllBrowsersPerTest extends SuiteMixin with WebBrowser with Eventually with
   implicit def webDriver: WebDriver = synchronized { privateWebDriver }
 
   /**
-   * Override <code>withFixture</code> to create new instance of <code>WebDriver</code> before
-   * running each test.  If there is error when creating <code>WebDriver</code>, <code>NoDriver</code>
-   * will be used and all tests will be canceled automatically.  If <code>WebDirver</code> creation
-   * is successful, a new instance of <code>TestServer</code> will be started for each test before they
-   * are executed.
+   * Register shared tests.
+   *
+   * @param forBrowser the passed in <code>ForBrowser</code> instance, you must append <code>forBrowser.name</code> to all tests register here.
+   */
+  def registerSharedTests(forBrowser: ForBrowser): Unit
+
+  if (registerSharedTestsForFirefox) registerSharedTests(ForFirefox)
+  if (registerSharedTestsForSafari) registerSharedTests(ForSafari)
+  if (registerSharedTestsForInternetExplorer) registerSharedTests(ForInternetExplorer)
+  if (registerSharedTestsForChrome) registerSharedTests(ForChrome)
+  if (registerSharedTestsForHtmlUnit) registerSharedTests(ForHtmlUnit)
+
+  private def mergeMap[A, B](ms: List[Map[A, B]])(f: (B, B) => B): Map[A, B] =
+    (Map[A, B]() /: (for (m <- ms; kv <- m) yield kv)) { (a, kv) =>
+      a + (if (a.contains(kv._1)) kv._1 -> f(a(kv._1), kv._2) else kv)
+    }
+
+  /**
+   * Overriden to automatically tag browser tests with browser tags.  Note that the browser tags will be merged with result returned from
+   * <code>super.tags</code>.
+   *
+   * @return <code>super.tags</code> with additional browser tags automatically
+   */
+  abstract override def tags: Map[String, Set[String]] = {
+    val generatedBrowserTags: Map[String, Set[String]] = Map.empty ++ testNames.map { tn =>
+      browsers.find(b => tn.endsWith(b.name)) match {
+        case Some(b) => (tn, Set(b.tagName))
+        case None => (tn, Set.empty[String])
+      }
+    }
+    mergeMap(List(super.tags, generatedBrowserTags.filter(!_._2.isEmpty))) { case (s1, s2) =>
+      s1 ++ s2  // just add the 2 sets together
+    }
+  }
+
+  /**
+   * Override <code>withFixture</code> to check <code>WebDriver</code> before running each test.  If it is a
+   * <code>NoDriver</code> all tests will be canceled automatically.  If valid <code>WebDriver</code> is available,
+   * a new instance of <code>TestServer</code> will be started for each test before they are executed.
    *
    * @param test the no-arg test function to run with a fixture
    * @return the <code>Outcome</code> of the test execution
    */
   abstract override def withFixture(test: NoArgTest): Outcome =
-    privateWebDriver match {
+    webDriver match {
       case NoDriver(ex) =>
-        val name = test.configMap("webDriverName")
-        val message = Resources("cantCreateDriver", name)
+        val name = test.configMap.getRequired[String]("webDriverName")
+        val message = Resources("cantCreateDriver", name.trim)
         ex match {
           case Some(e) => Canceled(message, e)
           case None => Canceled(message)
@@ -120,86 +259,30 @@ trait AllBrowsersPerTest extends SuiteMixin with WebBrowser with Eventually with
    * @return a <code>Status</code> object that indicates when the test started by this method has completed, and whether or not it failed .
    */
   abstract override def runTest(testName: String, args: Args): Status = {
-    val (name, driverFun) = privateDriverMeta
+    // looks at the end of the test name, and if it is one of the blessed ones,
+    // sets the port, driver, etc., before, and cleans up after, calling super.runTest
     synchronized {
       privateApp = new FakeApplication()
-      privateWebDriver =
-        try {
-          driverFun()
+      val (theWebDriver, theWebDriverName) =
+        browsers.find(b => testName.endsWith(b.name)) match {
+          case Some(b) => (b.createWebDriver, b.name)
+          case None => (WithoutDriver, "WithoutDriver")
         }
-        catch {
-          case ex: Throwable => NoDriver(Some(ex))
-        }
+      privateWebDriver = theWebDriver
+      privateWebDriverName = theWebDriverName
     }
     try {
-      val newConfigMap = args.configMap + ("app" -> privateApp) + ("port" -> port) + ("webDriver" -> webDriver) + ("webDriverName" -> name)
+      val newConfigMap = args.configMap + ("app" -> app) + ("port" -> port) + ("webDriver" -> webDriver) + ("webDriverName" -> privateWebDriverName)
       val newArgs = args.copy(configMap = newConfigMap)
       super.runTest(testName, newArgs)
     }
     finally {
-      privateWebDriver match {
+      webDriver match {
         case NoDriver(_) => // do nothing
-        case _ => privateWebDriver.close()
+        case WithoutDriver => // do nothing
+        case theDriver => theDriver.close()
       }
     }
   }
 
-  /**
-   * Override <code>run</code> to discover available browsers on the running platform (and filter if -Dbrowsers=... is passed in)
-   * before running the tests.  <code>super.run</code> will then be called for each targeted browser to run the tests.
-   *
-   * @param testName the name of one test to run.
-   * @param args the <code>Args</code> for this run
-   * @return a <code>Status</code> object that indicates when the test started by this method has completed, and whether or not it failed .
-   */
-  abstract override def run(testName: Option[String], args: Args): Status = {
-    val filterWebDrivers =
-      args.configMap.getOptional[String]("browsers") match {
-        case Some("") =>
-          args.reporter(AlertProvided(
-            args.tracker.nextOrdinal(),
-            Resources("emptyBrowsers"),
-            Some(NameInfo(this.suiteName, this.suiteId, Some(this.getClass.getName), testName))
-          ))
-          webDrivers
-
-        case Some(browsers) =>
-          val invalidChars = browsers.filter(c => !"CFISH".contains(c.toString.toUpperCase))
-          if (!invalidChars.isEmpty) {
-            val (resourceName, charsString) =
-              if (invalidChars.length > 1) {
-                val initString = invalidChars.init.map(c => "'" + c + "'").mkString(Resources("commaSpace"))
-                ("invalidBrowsersChars", Resources("and", initString, "'" + invalidChars.last  + "'"))
-              }
-              else
-                ("invalidBrowsersChar", "'" + invalidChars.head + "'")
-            args.reporter(AlertProvided(
-              args.tracker.nextOrdinal(),
-              Resources(resourceName, charsString),
-              Some(NameInfo(this.suiteName, this.suiteId, Some(this.getClass.getName), testName))
-            ))
-          }
-          val filteredDrivers =
-            webDrivers.filter { case (name, webDriverFun) =>
-              browsers.toUpperCase.contains(name.charAt(0))
-            }
-
-          // If no valid option, just fallback to default that uses all available browsers
-          if (filteredDrivers.isEmpty)
-            webDrivers
-          else
-            filteredDrivers
-
-        case None => webDrivers
-      }
-
-    new CompositeStatus(
-      filterWebDrivers.map { case (name, driverFun) =>
-        synchronized {
-          privateDriverMeta = (name, driverFun)
-        }
-        super.run(testName, args)
-      }
-    )
-  }
 }
