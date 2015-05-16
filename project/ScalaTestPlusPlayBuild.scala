8e02c5a99223e40b50bd7f2cfa987a637b00a1a8
cheeseng <cheeseng@amaseng.com>
2015-05-16 13:22:16 +0800

    Bump up selenium version to 2.45.

--- project/ScalaTestPlusPlayBuild.scala
+++ project/ScalaTestPlusPlayBuild.scala
@@ -67,7 +67,7 @@ object ScalaTestPlusPlayBuild extends Build {
     libraryDependencies ++= Seq(
       "org.scalatest" %% "scalatest" % "2.2.0",
       "com.typesafe.play" %% "play-test" % "2.4.0-RC3",
-      "org.seleniumhq.selenium" % "selenium-java" % "2.44.0",
+      "org.seleniumhq.selenium" % "selenium-java" % "2.45.0",
       "com.typesafe.play" %% "play-ws" % "2.4.0-RC3"
     ),
 
