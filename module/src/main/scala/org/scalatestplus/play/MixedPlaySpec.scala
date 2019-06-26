5b6655ddb39f336d60a1b9403183f5230cb1e37f
Alexander Van Hecke <alexander@avhconsult.be>
2019-06-26 17:29:37 +0200

    fixes #170 deprecated WebBrowser is used

--- module/src/main/scala/org/scalatestplus/play/MixedPlaySpec.scala
+++ module/src/main/scala/org/scalatestplus/play/MixedPlaySpec.scala
@@ -17,7 +17,6 @@ package org.scalatestplus.play
 
 import play.api.test._
 import org.scalatest._
-import selenium.WebBrowser
 import concurrent.Eventually
 import concurrent.IntegrationPatience
 import org.openqa.selenium.WebDriver
